9c5526db2228fe0dd9a826889eccd42d6bca96cd
C803518 <C803518@copel.nt>
2024-09-05 10:19:07 -0300

    projeto inicial

---
+++ rest-spring-aws/src/main/java/com/curso/erudio/rest_spring_aws/RestSpringAwsApplication.java
@@ -0,0 +1,13 @@
+package com.curso.erudio.rest_spring_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringAwsApplication.class, args);
+	}
+
+}
---
+++ rest-spring-aws/src/test/java/com/curso/erudio/rest_spring_aws/RestSpringAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.erudio.rest_spring_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
